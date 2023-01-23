8a949807ecd8ac851530b4b9e17cea267ad3dd3e
juantadaymalan3 <juantadaymalan3@gmail.com>
2023-01-23 11:47:42 -0500

    remove dist

--- dist/utils/error.handle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (res, error, errorRow) => {
-    console.log(errorRow);
-    res.status(500);
-    res.send({ error });
-};
-exports.handleHttp = handleHttp;
